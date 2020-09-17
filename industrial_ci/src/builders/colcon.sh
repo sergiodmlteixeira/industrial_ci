4aec4c5446098243d21e5feced02a269e1330554
Sérgio Daniel Marinho <sergio.d.marinho@inesctec.pt>
2020-09-17 10:41:33 +0100

    Fix

--- industrial_ci/src/builders/colcon.sh
+++ industrial_ci/src/builders/colcon.sh
@@ -67,7 +67,7 @@ function builder_test_results {
 
 function bundle_setup {
     wget http://packages.osrfoundation.org/gazebo.key -O - | apt-key add -
-    source /opt/ros/$BUILDER/setup.bash
+    source /opt/ros/$ROS_DISTRO/setup.bash
     ici_asroot pip3 install colcon-ros-bundle
 }
 
